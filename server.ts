6aed06d098ce64d53c26f740bdc3393beca0cbfe
Emiliano Caballero <emiliano.caballero98@gmail.com>
2023-10-02 17:23:20 -0300

    dotenv added, still working

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+const { PORT } = process.env;
 const app = express();
 
 app.get("/", (_req, res) => {
   res.send('hola mundo');
 });
 
-app.listen( 8080, () => {
+app.listen( PORT, () => {
   console.log("Server is running on port 8080");
 });
